6edb7e0690eff846906619a7f618328a4431ecef
Mike Fiore <mfiore@multitech.com>
2016-06-23 16:00:21 -0500

    refactored unit table into HTML, added updates to unit stats based on race selection

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
 var my_techs;
-
-class unit {
-	constructor(name, cost, battle, mvmt, special) {
-		this.name = name;
-		this.cost = cost;
-		this.battle = battle;
-		this.mvmt = mvmt;
-		this.special = special;
-	}
-}
+var my_race = "";
 
 var races = [
 	"The Barony of Letnev",
@@ -20,43 +11,62 @@ var races = [
 	"Sardakk N'orr",
 	"The Universities of Jol-Nar",
 	"The Xxcha Kingdom",
-	"The Yssaril Tribes"
-];
-
-var my_units = [
-	new unit("Dreadnought", "5", "5", "1", "Bombardment, Sustain Damage"),
-	new unit("Carrier", "3", "9", "1", "Capacity: 6"),
-	new unit("Cruiser", "2", "7", "2", ""),
-	new unit("Destroyer", "1", "9", "2", "Anti-Fighter Barrage"),
-	new unit("Fighter (x2)", "1", "9", "n/a", ""),
-	new unit("War Sun", "12", "3 (x3)", "2", "Bombardment, Capacity: 6, Sustain Damage"),
-	new unit("Ground Force (x2)", "1", "8", "-", ""),
-	new unit("PDS", "1", "6", "-", "Planetary Shield, Space Cannon"),
-	new unit("Space Dock", "4", "-", "-", "Produce Units, Fighter Capacity: 3")
+	"The Yssaril Tribes",
+	"The Yin Brotherhood",
+	"The Embers of Muaat",
+	"The Clan of Saar"
 ];
 
 function raceSelected(race) {
+	my_race = race.value;
+	document.getElementById('header').style.display = 'block';
+	document.getElementById('header-title').innerText = my_race;
+
 	document.getElementById('race-div').style.display = 'none';
 	document.getElementById('unit-div').style.display = 'block';
-	var table = document.getElementById('unit-table');
-	
-	for (var i = 0; i < my_units.length; i++) {
-		var row = table.insertRow();
-		var cell0 = row.insertCell(0);
-		var text0 = document.createTextNode(my_units[i].name);
-		cell0.appendChild(text0);
-		var cell1 = row.insertCell(1);
-		var text1 = document.createTextNode(my_units[i].cost);
-		cell1.appendChild(text1);
-		var cell2 = row.insertCell(2);
-		var text2 = document.createTextNode(my_units[i].battle);
-		cell2.appendChild(text2);
-		var cell3 = row.insertCell(3);
-		var text3 = document.createTextNode(my_units[i].mvmt);
-		cell3.appendChild(text3);
-		var cell4 = row.insertCell(4);
-		var text4 = document.createTextNode(my_units[i].special);
-		cell4.appendChild(text4);
+
+	//jon-nar -1 on all combat rolls
+	if (my_race === "The Universities of Jol-Nar") {
+		var table = document.getElementById('unit-table');
+		for (var i = 1; i < table.rows.length; i++) {
+			var current = parseInt(table.rows[i].cells[2].innerHTML);
+			if (! isNaN(current)) {
+				table.rows[i].cells[2].innerHTML = current + 1;
+			}
+		}
+	}
+	//sardakk n'orr +1 on all combat rolls
+	if (my_race === "Sardakk N'orr") {
+		var table = document.getElementById('unit-table');
+		for (var i = 1; i < table.rows.length; i++) {
+			var current = parseInt(table.rows[i].cells[2].innerHTML);
+			if (! isNaN(current)) {
+				table.rows[i].cells[2].innerHTML = current - 1;
+			}
+		}
+	}
+	//embers of muaat war suns base movement of 1, upgraded when deep space cannon is acquired
+	if (my_race === "The Embers of Muaat") {
+		var unit = document.getElementById('unit-war-sun');
+		unit.cells[4].innerHTML = 1;
+	}
+	//l1z1x mindnet dreadnought base cost of 4 and +1 during space battles
+	if (my_race === "The L1Z1X Mindnet") {
+		var unit = document.getElementById('unit-dreadnought');
+		unit.cells[1].innerHTML = 4;
+		var current = parseInt(unit.cells[2].innerHTML);
+		unit.cells[2].innerHTML = current - 1;
+	}
+	//naalu collective fighters +1 on combat rolls
+	if (my_race === "The Naalu Collective") {
+		var unit = document.getElementById('unit-fighter');
+		var current = parseInt(unit.cells[2].innerHTML);
+		unit.cells[2].innerHTML = current - 1;
+	}
+	//clan of saar space docks have 1 movement
+	if (my_race === "The Clan of Saar") {
+		var unit = document.getElementById('unit-space-dock');
+		unit.cells[4].innerHTML = 1;
 	}
 }
 
